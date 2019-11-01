857d4e0b70c2b56b6b71a6f6947746f4cb6eb4ec
Timur <sol485@outlook.com>
2019-11-01 13:09:06 +0300

    35 Обработка событий

--- src/index.js
+++ src/index.js
@@ -1,27 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header.js';
-import SearchPanel from './components/search-panel.js';
-import TodoList from './components/todo-list.js';
-
-
-const App = () => {
-
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1},
-    { label: 'Make Awesome App', important: true, id: 2},
-    { label: 'Have a lunch', important: false, id: 3}
-  ];
-
-  return (
-    <div>
-      <AppHeader/>
-      <SearchPanel/>
-      <TodoList todos={todoData} />
-    </div>
-  );
-};
+import App from './components/app';
 
 ReactDOM.render(<App />,
   document.getElementById('root'));
