90cde07cd7e1bdde999195069948dc15b64c9175
Timur <sol485@outlook.com>
2020-01-08 15:25:47 +0300

    Add form to 'add item' feature and finish it

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-import AppHeader from '../app-header/app-header';
+import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
-import ItemStatusFilter from '../item-status-filter/item-status-filter';
+import ItemStatusFilter from '../item-status-filter';
 import ItemAddForm from '../item-add-form';
 
 import './app.css';
@@ -14,19 +14,28 @@ export default class App extends Component {
 
   state = {
     todoData: [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
+      this.createTodoItem('Drink Coffee'),
+      this.createTodoItem('Make Awesome App'),
+      this.createTodoItem('Have a lunch')
     ]
   };
 
+  createTodoItem(label) {
+    return {
+      label,
+      important: false,
+      done: false,
+      id: this.maxId++
+    }
+  }
+
   deleteItem = (id) => {
-    this.setState(({ todoData  }) => {
-      const indx = todoData.findIndex((el) => el.id === id);
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id);
 
       const newArray = [
-        ...todoData.slice(0, indx),
-        ...todoData.slice(indx + 1)
+        ...todoData.slice(0, idx),
+        ...todoData.slice(idx + 1)
       ];
 
       return {
@@ -36,50 +45,75 @@ export default class App extends Component {
   };
 
   addItem = (text) => {
-    const newItem = {
-      label: text,
-      important: false,
-      id: this.maxId++
-    };
+    const newItem = this.createTodoItem(text);
 
     this.setState(({ todoData }) => {
-      const newArray = [
+      const newArr = [
         ...todoData,
         newItem
       ];
 
       return {
-        todoData: newArray
-      }
+        todoData: newArr
+      };
+    });
+
+  };
+
+  toggleProperty(arr, id, propName) {
+    const idx = arr.findIndex((el) => el.id === id);
+
+    const oldItem = arr[idx];
+    const newItem = {...oldItem,
+      [propName]: !oldItem[propName]};
+
+    return [
+      ...arr.slice(0, idx),
+      newItem,
+      ...arr.slice(idx + 1)
+    ];
+  }
+
+  onToggleDone = (id) => {
+    this.setState(({ todoData }) => {
+      return {
+        todoData: this.toggleProperty(todoData, id, 'done')
+      };
     });
   };
 
   onToggleImportant = (id) => {
-    console.log("Toggle Important", id);
-  };
-
-  onToggleDone = (id) => {
-    console.log("Toggle Done", id);
+    this.setState(({ todoData }) => {
+      return {
+        todoData: this.toggleProperty(todoData, id, 'important')
+      };
+    });
   };
 
   render() {
+
+    const { todoData } = this.state;
+    const doneCount = todoData
+                      .filter((el) => el.done).length;
+    const todoCount = todoData.length - doneCount;
+
     return (
       <div className="todo-app">
-        <AppHeader toDo={1} done={3} />
+        <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
           <SearchPanel />
           <ItemStatusFilter />
         </div>
 
         <TodoList
-          todos={this.state.todoData}
-          onDeleted={this.deleteItem}
+          todos={todoData}
+          onDeleted={ this.deleteItem }
           onToggleImportant={this.onToggleImportant}
           onToggleDone={this.onToggleDone}
         />
+
         <ItemAddForm onItemAdded={this.addItem}/>
       </div>
     );
   }
 };
-
--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -4,15 +4,32 @@ import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
 
+  state = {
+    label: " "
+  };
+  onLabelChange = (evt) => {
+    this.setState({
+      label: evt.target.value
+    });
+  };
+
+  onSubmit = (evt) => {
+    evt.preventDefault();
+    this.props.onItemAdded(this.state.label);
+  };
+
   render() {
     return (
-      <div className="item-add-form ">
-        <button type="button"
-                className="btn btn-primary"
-                onClick={() => this.props.onItemAdded('Hello')}>
+      <form className="item-add-form d-flex"
+            onSubmit={this.onSubmit}>
+        <input type="text"
+               className="form-control"
+               onChange={this.onLabelChange}
+               placeholder="What needs to be done" />
+        <button className="btn btn-primary">
           Add Item
         </button>
-      </div>
+      </form>
     )
   }
 }
